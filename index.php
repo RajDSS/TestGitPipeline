85e9cea2b2b84c5360e7845a034bf37cb18228d8
Chanchal Raj <rajchanchalkhatri@gmail.com>
2020-06-11 14:55:04 +0400

    Added connection file

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<h1>سائين مهرباني اچڻ لا</h1>'; ?> 
- </body>
-</html>
+<?php
+
+/* Include the MySQLi procedural-style connection script */
+include 'connection.php';
+$query = "SELECT * FROM User";
+/* Execute the query */
+$result = mysqli_query($conn, $query);
+
+/* Check for errors */
+if (!$result)
+{
+   echo 'Query error: ' . mysqli_error($conn);
+   die();
+}
+
+/* Iterate through the result set */
+while ($row = mysqli_fetch_assoc($result))
+{
+   echo 'User Name: ' . $row['user_name'] . ', Password: ' . $row['password'] . '<br>';
+}
+
+?>
